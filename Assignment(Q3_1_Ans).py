392b6872a3feb20951843a20ce9d4bf95ff0b979
KasunHewagama <44252824+KasunHewagama@users.noreply.github.com>
2020-08-26 12:47:30 +0530

    Added Assignment(Q3_1_Ans).py...!

---
+++ Assignment(Q3_1_Ans).py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 11:30:54 2020
+
+@author: Kasun_Hewagama
+@Sid: IT18165180
+@Batch: DS 2020
+"""
+#Entering the values that should permutate
+Term = input("Enter the term to rotate : ") 
+print("Term to be permutate",Term) 
+
+a = Term + '$'
+print(a)
+X = len(a)
+for i in range(X-1,0,-1):
+    c = a[i:]+a[:i]
+    print(c,Term)
+print(a,Term)
